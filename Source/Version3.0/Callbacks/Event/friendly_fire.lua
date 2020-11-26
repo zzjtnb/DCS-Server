3a03b36ea417fa2f269dde891a7ed8ebd15bf87a
zzjtnb <zhengzhujituan@gmail.com>
2020-11-26 20:25:57 +0800

    update

--- Source/Version3.0/Callbacks/Event/friendly_fire.lua
+++ Source/Version3.0/Callbacks/Event/friendly_fire.lua
@@ -8,7 +8,7 @@ SourceCall.friendly_fire = function(eventName, playerID, weaponName, victimPlaye
   -- net.log("第1个条件:", os.difftime(os.time(), SourceCall.PlayerInfo[ucid]["loginTime"]) < 1800, SourceCall.PlayerInfo[ucid]["KillFriend"] >= 2)
   -- net.log("第2个条件:", os.difftime(os.time(), SourceCall.PlayerInfo[ucid]["quitTime"]) > 1800, SourceCall.PlayerInfo[ucid]["KillFriend"] < 3)
   -- net.log("第3个条件:", os.difftime(os.time(), SourceCall.PlayerInfo[ucid]["loginTime"]) > 1800, SourceCall.PlayerInfo[ucid]["KillFriend"] > 3)
-  if os.difftime(os.time(), SourceCall.PlayerInfo[ucid]["loginTime"]) < 1800 and SourceCall.PlayerInfo[ucid]["KillFriend"] >= 2 then
+  if os.difftime(os.time(), SourceCall.PlayerInfo[ucid]["loginTime"]) < 1800 and SourceCall.PlayerInfo[ucid]["KillFriend"] == 2 then
     SourceCall.PlayerInfo[ucid]["quitTime"] = os.time()
     FileData.SaveData(SourceCall.PlayerInfoFile, net.lua2json(SourceCall.PlayerInfo))
     net.kick(playerID, "30分钟内击杀友军" .. tostring(SourceCall.PlayerInfo[ucid]["KillFriend"]) .. "次")
@@ -16,6 +16,8 @@ SourceCall.friendly_fire = function(eventName, playerID, weaponName, victimPlaye
     SourceCall.BannedClients[ucid] = {IP = SourceCall.PlayerInfo[ucid]["ipaddr"], ucid = SourceCall.PlayerInfo[ucid]["ucid"], name = SourceCall.PlayerInfo[ucid]["name"]}
     FileData.SaveData(SourceCall.BannedClientsFile, net.lua2json(SourceCall.BannedClients))
     net.kick(playerID, "30分钟内击杀友军" .. tostring(SourceCall.PlayerInfo[ucid]["KillFriend"]) .. "次,你已被永久封禁")
+    SourceCall.PlayerInfo[ucid]["KillFriend"] = 0
+    FileData.SaveData(SourceCall.PlayerInfoFile, net.lua2json(SourceCall.PlayerInfo))
   elseif os.difftime(os.time(), SourceCall.PlayerInfo[ucid]["quitTime"]) > 1800 and SourceCall.PlayerInfo[ucid]["KillFriend"] < 2 then
     SourceCall.PlayerInfo[ucid]["KillFriend"] = 0
     FileData.SaveData(SourceCall.PlayerInfoFile, net.lua2json(SourceCall.PlayerInfo))
