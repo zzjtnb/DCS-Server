d22e4c92d9fcd4dd671407b7b159bc6fa409a1a8
zzjtnb <zhengzhujituan@gmail.com>
2021-03-21 22:54:55 +0800

    update

--- Debug/version2.0/Debugger.lua
+++ Debug/version2.0/Debugger.lua
@@ -23,7 +23,7 @@ function Debugger.callbacks.onSimulationFrame()
       if TCP.server then
         TCP.client = TCP.server:accept() --等待任何客户端的连接
         if DCS.isServer() and TCP.client then
-          TCP.client:settimeout(10) --确保我们不会阻止等待这个客户端消息
+          -- TCP.client:settimeout(10) --确保我们不会阻止等待这个客户端消息
           -- local ip, port = TCP.server:getsockname() --获取连接信息
           local line, err = TCP.client:receive() --接收客户端传来的消息
           if not err then
